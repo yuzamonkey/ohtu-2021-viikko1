7cc5cf41937d567e3ca64f1eb5d3807cb05a812d
Jusa Mankki <jusamankki@gmail.com>
2021-11-10 12:20:35 +0200

    add lint to workflow, add lint error to test

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         self.tilavuus = max(0.0, tilavuus)
+        
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
